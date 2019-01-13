dc7a7b82f42791c46ee3d24b488c3e22387fe47f
Shusil <shusilshapkota@gmail.com>
2019-01-13 16:34:40 -0500

    Added profile functionality, records the rating and saves it to the database

---
+++ app/src/main/java/com/rate_your_assignment/StarterApplication.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2015-present, Parse, LLC.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree. An additional grant
+ * of patent rights can be found in the PATENTS file in the same directory.
+ */
+package com.rate_your_assignment;
+
+import android.app.Application;
+
+
+public class StarterApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+/*
+        //Parse.enableLocalDatastore(this);
+
+
+      //  Parse.initialize(new Parse.Configuration.Builder(getApplicationContext())
+         //Enter your application ID
+                .applicationId("pistagramrfdvhygfyyuhurfudhhr")
+                .clientKey(null)
+         // Enter the server URL
+                .server("http://pistagram123.herokuapp.com/parse/")
+                .build()
+        );
+
+        ParseACL defaultACL = new ParseACL();
+
+        ParseACL.setDefaultACL(defaultACL, true);*/
+    }
+}
